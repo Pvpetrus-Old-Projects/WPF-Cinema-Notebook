c0f9044f40a1a588ac077a98eea7fee2f13720e2
erpus <erpus@wp.pl>
2023-06-13 20:23:11 +0200

    poczatek robienia (poczatek listy notatek i edycji notatki)

---
+++ ProjektWPF/Pages/NoteDetails.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjektWPF.Pages
+{
+    /// <summary>
+    /// Interaction logic for NoteDetails.xaml
+    /// </summary>
+    public partial class NoteDetails : Page
+    {
+        ApplicationDatabaseEntities db = new ApplicationDatabaseEntities();
+        private List<Note> notesL;
+        Note note;
+        public int UserId { get; set; }
+        string mode;
+        public NoteDetails()
+        {
+            mode = "creation";
+            InitializeComponent();
+            note = new Note();
+            deleteNoteButton.Visibility = Visibility.Collapsed; //delete button not needed if the film is being added
+        }
+        public NoteDetails(int filmId, string title, DateTime date, int rating, string text)
+        {
+            mode = "edition";
+            InitializeComponent();
+            deleteNoteButton.Visibility = Visibility.Visible;
+            noteTitle.Text = title;
+            noteText.Text = text;
+            notesL = db.Note.ToList();
+            //note = notesL.Find(el => el.Id == id); //ND
+        }
+
+        private void saveAddFilm(object sender, RoutedEventArgs e, DateTime dateTime)
+        {
+
+            if (mode == "edition")
+            {
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                note.UserId = UserId;
+
+                try
+                {
+                    db.Note.Add(note);
+                    db.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            switchToFilmsList();
+        }
+
+        private void deleteFilm(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                db.Note.Remove(note);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            switchToFilmsList();
+        }
+
+        private void switchToFilmsList()
+        {
+            Notes notesList = new Notes(UserId);  //we have to create a new page to refresh the list
+            NavigationService navigationService = NavigationService.GetNavigationService(this);
+            navigationService.Navigate(notesList);
+        }
+    }
+}
---
+++ ProjektWPF/Pages/Notes.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjektWPF.Pages
+{
+    /// <summary>
+    /// Interaction logic for Films.xaml
+    /// </summary>
+    public partial class Notes : Page
+    {
+        ApplicationDatabaseEntities db = new ApplicationDatabaseEntities();
+        private List<Note> notesL;
+        public int UserId { get; set; }
+
+        public Notes(int userId)
+        {
+            InitializeComponent();
+            notesL = db.Note.ToList();
+            notesList.DataContext = notesL;
+            UserId = userId;
+        }
+
+        private void doubleClickOnNote(object sender, MouseButtonEventArgs e)
+        {
+            Note selectedElement = notesList.SelectedItem as Note;
+            NoteDetails noteDetails = new NoteDetails(); // ND
+            noteDetails.UserId = UserId; //set user data
+            NavigationService navigationService = NavigationService.GetNavigationService(this);
+            navigationService.Navigate(noteDetails);
+        }
+
+        private void addNote(object sender, RoutedEventArgs e)
+        {
+            NoteDetails notesDetails = new NoteDetails();
+            notesDetails.UserId = UserId; // ND
+            NavigationService navigationService = NavigationService.GetNavigationService(this);
+            navigationService.Navigate(notesDetails);
+        }
+
+        private void displayAllNotes()
+        {
+            notesList.DataContext = notesL;
+        }
+
+        private void searchFilm(object sender, TextChangedEventArgs e)
+        {
+            if (searchNoteInput.Text.Length > 1)
+                notesList.DataContext = notesL.Where(el => el.NoteTitle.ToLower().Contains(searchNoteInput.Text)).ToList();
+            else displayAllNotes();
+        }
+
+        private void notesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
